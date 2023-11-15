9f4b83393a5697758734c8da0b3d323fcc883743
xinweitang <txw19881222@126.com>
2023-11-15 15:36:42 +0800

    修改编译标记

--- node-script/replace_stackprotector.js
+++ node-script/replace_stackprotector.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 var lines = fs.readFileSync(process.argv[2], 'utf-8').split(/[\n\r]/);
 
 for(var i = 0; i < lines.length; i++) {
-    lines[i] = lines[i].replace("-fstack-protector", "-fno-stack-protector");
+    //lines[i] = lines[i].replace("-fstack-protector", "-fno-stack-protector");
+    lines[i] = lines[i].replace(`fortify_level = "2"`, `fortify_level = "1"`);
 }
 
 console.log(`write to file ${process.argv[2]}`)
