575ae503125f06fb35753b2a260431776de0ab2f
xinweitang <txw19881222@126.com>
2023-11-17 23:55:58 +0800

    fix

--- node-script/changeclang_version.js
+++ node-script/changeclang_version.js
@@ -4,7 +4,7 @@ var lines = fs.readFileSync(process.argv[2], 'utf-8').split(/[\n\r]/);
 
 for(var i = 0; i < lines.length; i++) {
     //lines[i] = lines[i].replace("-fstack-protector", "-fno-stack-protector");
-    lines[i] = lines[i].replace(`17`, `11`).replace(`18`, `11`);
+    lines[i] = lines[i].replace(`17`, `11.0.0`).replace(`18`, `11.0.0`);
 }
 
 console.log(`write to file ${process.argv[2]}`)
