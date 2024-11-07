032dd8388ab04026d7b443108a90d1f9a9c6882a
tangxinwei <txw19881222@126.com>
2024-11-07 15:48:22 +0800

    补充iios simulate

---
+++ ios_simulator.sh
@@ -0,0 +1,49 @@
+VERSION=$1
+[ -z "$GITHUB_WORKSPACE" ] && GITHUB_WORKSPACE="$( cd "$( dirname "$0" )"/.. && pwd )"
+
+cd ~
+echo "=====[ Getting Depot Tools ]====="	
+git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
+cd depot_tools
+git reset --hard 8d16d4a
+cd ..
+export DEPOT_TOOLS_UPDATE=0
+export PATH=$(pwd)/depot_tools:$PATH
+gclient
+
+
+mkdir v8
+cd v8
+
+echo "=====[ Fetching V8 ]====="
+fetch v8
+echo "target_os = ['ios']" >> .gclient
+cd ~/v8/v8
+git checkout refs/tags/$VERSION
+gclient sync
+
+
+# echo "=====[ Patching V8 ]====="
+# git apply --cached $GITHUB_WORKSPACE/patches/builtins-puerts.patches
+# git checkout -- .
+
+if [ "$VERSION" == "11.8.172" ] || [ "$VERSION" == "11.8.172.18" ] || [ "$VERSION" == "11.8.172.18-pgo" ]; then
+  node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/remove_uchar_include_v11.8.172.patch
+fi
+
+echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
+node $GITHUB_WORKSPACE/node-script/add_arraybuffer_new_without_stl.js .
+
+node $GITHUB_WORKSPACE/node-script/patchs.js . $VERSION
+
+echo "=====[ Building V8 ]====="
+ 
+gn gen out.gn/x64.release --args="v8_use_external_startup_data=false v8_use_snapshot=true v8_enable_i18n_support=false is_debug=false v8_static_library=true ios_enable_code_signing=false strip_debug_info=true symbol_level=0 target_os=\"ios\" target_cpu=\"x64\" v8_enable_pointer_compression=true libcxx_abi_unstable=false v8_enable_sandbox=false use_custom_libcxx=false v8_enable_maglev=false"
+
+ninja -C out.gn/x64.release -t clean
+ninja -C out.gn/x64.release wee8
+strip -v -S out.gn/x64.release/obj/libwee8.a
+
+mkdir -p output/v8/Lib/iOS/x64
+cp out.gn/x64.release/obj/libwee8.a output/v8/Lib/iOS/x64/
+mkdir -p output/v8/Inc/Blob/iOS/x64
