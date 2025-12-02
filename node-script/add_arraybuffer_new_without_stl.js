d473912d35173b7b878c443b01437267c5517ff2
tangxinwei <txw19881222@126.com>
2025-12-02 22:32:20 +0800

    fix

--- node-script/add_arraybuffer_new_without_stl.js
+++ node-script/add_arraybuffer_new_without_stl.js
@@ -27,7 +27,7 @@ V8_EXPORT void* ArrayBuffer_Get_Data(Local<ArrayBuffer> array_buffer);
 if(process.argv[3] == 'true'){
     v8_h_insert_code = v8_h_insert_code + `
 #define PUERTS_V8_USE_CUSTOM_CXX 1
-#inlcude "v8-inspector.h"
+#include "v8-inspector.h"
 #include "libplatform/libplatform.h"
 namespace v8
 {
@@ -44,7 +44,7 @@ V8_EXPORT int Wrapper_NewDefaultPlatform(int thread_pool_size = 0,
 V8_EXPORT void PuertsReleasePlatform(int Index);
 }
 
-V8_EXPORT int Wrapper_Inspector_Create(Isolate*, V8InspectorClient*);
+V8_EXPORT int Wrapper_Inspector_Create(Isolate*, v8_inspector::V8InspectorClient*);
 V8_EXPORT void PuertsReleaseInspector(int Index);
 }
     `
@@ -57,7 +57,7 @@ fs.writeFileSync(v8_h_path, v8_h_context.slice(0, v8_h_insert_pos) + v8_h_insert
 let api_cc_path = process.argv[2] + '/src/api/api.cc';
 
 let api_cc_insert_code = `
-#include "include/v8-version.h"
+#include "include/v8.h"
 namespace v8
 {
 V8_EXPORT Local<ArrayBuffer> ArrayBuffer_New_Without_Stl(Isolate* isolate, 
@@ -110,88 +110,109 @@ if(process.argv[3] == 'true')
     api_cc_insert_code = api_cc_insert_code + `
 namespace v8
 {
-static std::vector<std::shared_ptr<BackingStore> > _cached_backing_store;
-static std::vector<int> free_cached_backing_store_list;
+static std::vector<std::shared_ptr<BackingStore> >* _cached_backing_store = nullptr;
+static std::vector<int>* free_cached_backing_store_list = nullptr;
 V8_EXPORT int ArrayBuffer_KeepBackingStore(Local<ArrayBuffer> array_buffer)
 {
-  std::shared_ptr<BackingStore> bs = array_buffer->GetBackingStore();
-  if(free_cached_backing_store_list.size())
+  if(!_cached_backing_store)
   {
-    int ret = free_cached_backing_store_list.back();
-    free_cached_backing_store_list.pop_back();
-    _cached_backing_store[ret] = bs;
+    _cached_backing_store = new std::vector<std::shared_ptr<BackingStore> >();
+    free_cached_backing_store_list = new std::vector<int>();
+  }
+  std::shared_ptr<BackingStore> bs = array_buffer->GetBackingStore();
+  if(free_cached_backing_store_list->size())
+  {
+    int ret = free_cached_backing_store_list->back();
+    free_cached_backing_store_list->pop_back();
+    (*_cached_backing_store)[ret] = bs;
     return ret;
   }
-  _cached_backing_store.push_back(bs);
-  return _cached_backing_store.size() - 1;
+  _cached_backing_store->push_back(bs);
+  return (int)_cached_backing_store->size() - 1;
 }
 
 V8_EXPORT int ArrayBuffer_KeepBackingStore(Local<SharedArrayBuffer> array_buffer)
 {
-  std::shared_ptr<BackingStore> bs = array_buffer->GetBackingStore();
-  if(free_cached_backing_store_list.size())
+  if(!_cached_backing_store)
   {
-    int ret = free_cached_backing_store_list.back();
-    free_cached_backing_store_list.pop_back();
-    _cached_backing_store[ret] = bs;
+    _cached_backing_store = new std::vector<std::shared_ptr<BackingStore> >();
+    free_cached_backing_store_list = new std::vector<int>();
+  }
+  std::shared_ptr<BackingStore> bs = array_buffer->GetBackingStore();
+  if(free_cached_backing_store_list->size())
+  {
+    int ret = free_cached_backing_store_list->back();
+    free_cached_backing_store_list->pop_back();
+    (*_cached_backing_store)[ret] = bs;
     return ret;
   }
-  _cached_backing_store.push_back(bs);
-  return _cached_backing_store.size() - 1;
+  _cached_backing_store->push_back(bs);
+  return (int)_cached_backing_store->size() - 1;
 }
 
 V8_EXPORT void PuertsReleaseBackingStore(int Index)
 {
-  _cached_backing_store[Index].reset();
-  free_cached_backing_store_list.push_back(Index);
+  (*_cached_backing_store)[Index].reset();
+  free_cached_backing_store_list->push_back(Index);
 }
 
 namespace platform {
-static std::vector<std::unique_ptr<v8::Platform> > _cached_platform;
-static std::vector<int> free_cached_platform;
+static std::vector<std::unique_ptr<v8::Platform> >* _cached_platform = nullptr;
+static std::vector<int>* free_cached_platform = nullptr;
 V8_EXPORT int Wrapper_NewDefaultPlatform(int thread_pool_size, IdleTaskSupport idle_task_support, InProcessStackDumping in_process_stack_dumping,PriorityMode priority_mode)
 {
-  std::unique_ptr<v8::Platform> p = NewDefaultPlatform(thread_pool_size, idle_task_support, in_process_stack_dumping, priority_mode);
-  if(free_cached_platform.size())
+  if(!_cached_platform)
   {
-    int ret = free_cached_platform.back();
-    free_cached_platform.pop_back();
-    _cached_platform[ret] = std::move(p);
+    _cached_platform = new std::vector<std::unique_ptr<v8::Platform> >();
+    free_cached_platform = new std::vector<int>();
+  }
+  std::unique_ptr<v8::Platform> p = NewDefaultPlatform(thread_pool_size, idle_task_support, in_process_stack_dumping, nullptr, priority_mode);
+  if(free_cached_platform->size())
+  {
+    int ret = free_cached_platform->back();
+    free_cached_platform->pop_back();
+    (*_cached_platform)[ret] = std::move(p);
     return ret;
   }
-  _cached_platform.push_back(std::move(p));
-  return _cached_platform.size() - 1;
+  _cached_platform->push_back(std::move(p));
+  return (int)_cached_platform->size() - 1;
 }
 
 V8_EXPORT void PuertsReleasePlatform(int Index)
 {
-  _cached_platform[Index].reset();
-  free_cached_platform.push_back(Index);
+  (*_cached_platform)[Index].reset();
+  free_cached_platform->push_back(Index);
 }
 
 }
 
-static std::vector<std::unique_ptr<V8Inspector> > _cached_inspector;
-static std::vector<int> free_cached_inspector;
-V8_EXPORT int Wrapper_Inspector_Create(Isolate* Isolate, V8InspectorClient* Client)
+static std::vector<std::unique_ptr<v8_inspector::V8Inspector> >* _cached_inspector = nullptr;
+static std::vector<int>* free_cached_inspector = nullptr;
+V8_EXPORT int Wrapper_Inspector_Create(Isolate* Isolate, v8_inspector::V8InspectorClient* Client)
 {
-  std::unique_ptr<V8Inspector> s = V8Inspector::create(Isolate, Client);
-  if(free_cached_inspector.size())
+  if(!_cached_inspector)
   {
-    int ret = free_cached_inspector.back();
-    free_cached_inspector.pop_back();
-    _cached_inspector[ret] = std::move(s);
+    _cached_inspector = new std::vector<std::unique_ptr<v8_inspector::V8Inspector> >();
+    free_cached_inspector = new std::vector<int>();
+  }
+  std::unique_ptr<v8_inspector::V8Inspector> s = v8_inspector::V8Inspector::create(Isolate, Client);
+  if(free_cached_inspector->size())
+  {
+    int ret = free_cached_inspector->back();
+    free_cached_inspector->pop_back();
+    (*_cached_inspector)[ret] = std::move(s);
     return ret;
   }
-  _cached_inspector.push_back(std::move(s));
-  return _cached_inspector.size() - 1;
+  _cached_inspector->push_back(std::move(s));
+  return (int)_cached_inspector->size() - 1;
 }
 
 V8_EXPORT void PuertsReleaseInspector(int Index)
 {
-  _cached_inspector[Index].reset();
-  free_cached_inspector.push_back(Index);
+  (*_cached_inspector)[Index].reset();
+  free_cached_inspector->push_back(Index);
 }
+
 }
     `
 }
